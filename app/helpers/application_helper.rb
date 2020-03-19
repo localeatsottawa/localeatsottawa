4c663fb38406b19bc3c2bdecd952b2a6c81148d9
Casey Li <casey.li@gmail.com>
2020-03-18 22:44:15 -0400

    Added icons.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def check_icon(value)
+    if value
+      content_tag :i, '', class: 'fas fa-check-circle success'
+    else
+      content_tag :i, '', class: 'fas fa-times-circle error'
+    end
+  end
+  
 end
