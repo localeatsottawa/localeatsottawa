945bfc0a69fbf4aa1ee935243f2fab63430cdbe3
Pascal Laliberté <pascal@pascallaliberte.me>
2020-03-22 19:41:52 -0400

    Use before_save for ensure_url_has_protocol instead of accessor override

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,21 +1,17 @@
 class Restaurant < ApplicationRecord
+  before_save :ensure_url_has_protocol
+  
   has_many :locations, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 
   scope :alphabetical, -> { order(name: :asc)}
   
-  def website
-    ensure_url_has_protocol read_attribute(:website)
-  end
-  
   private
   
-  def ensure_url_has_protocol(website)
-    if ( website !~ /https?:\/\// && !website.to_s.strip.empty? )
-      "http://" + website
-    else
-      website
+  def ensure_url_has_protocol
+    if ( self.website !~ /^https?:\/\// && !self.website.to_s.strip.empty? )
+      self.website = "http://" + self.website
     end
   end
 end
