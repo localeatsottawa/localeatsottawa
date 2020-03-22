6632a5da9febcac1625cda730cc8bc550e2c727c
Casey Li <casey.li@gmail.com>
2020-03-22 12:55:27 -0400

    Alphabetized restaurants index in API.

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::RestaurantsController < Api::V1::ApplicationController
   load_and_authorize_resource
   
   def index
+    @restaurants = @restaurants.alphabetical
+    
     respond_to do |format|
       format.json
     end
