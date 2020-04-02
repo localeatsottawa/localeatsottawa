132779e03a0e01fa255c5fee7bee4d47fad7424e
Casey Li <casey.li@gmail.com>
2020-04-01 23:24:09 -0400

    Finished sending filters to get request.

--- app/javascript/packs/restaurants.jsx
+++ app/javascript/packs/restaurants.jsx
@@ -9,6 +9,13 @@ import Restaurant from '../components/restaurant';
 class Restaurants extends React.Component {
   state = {
     loadingRestaurants: true,
+    showFilters: true,
+    filterPickup: true,
+    filterDelivery: true,
+    filterUberEats: true,
+    filterSkipTheDishes: true,
+    filterFoodora: true,
+    filterDoorDash: true,
     restaurants: []
   }
 
@@ -17,15 +24,141 @@ class Restaurants extends React.Component {
   }
 
   loadRestaurants = () => {
-    $.getJSON('/restaurants', (restaurants) => {
+    this.setState({ loadingRestaurants: true, showFilters: false });
+
+    const {
+      filterPickup: pickup,
+      filterDelivery: delivery,
+      filterUberEats: uber_eats,
+      filterSkipTheDishes: skip_the_dishes,
+      filterFoodora: foodora,
+      filterDoorDash: door_dash,
+    } = this.state;
+
+    const data = {
+      pickup,
+      delivery,
+      uber_eats,
+      skip_the_dishes,
+      foodora,
+      door_dash,
+    }
+
+    $.getJSON('/restaurants', data, (restaurants) => {
       this.setState({restaurants, loadingRestaurants: false});
     });
   }
 
+  toggleFilters = (e) => {
+    e.preventDefault();
+
+    const { showFilters } = this.state;
+    this.setState({ showFilters: !showFilters });
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
-    const { restaurants, loadingRestaurants } = this.state;
+    const { 
+      restaurants, 
+      loadingRestaurants, 
+      showFilters,
+      filterPickup,
+      filterDelivery,
+      filterUberEats,
+      filterSkipTheDishes,
+      filterFoodora,
+      filterDoorDash,
+    } = this.state;
     return (
       <div className='component-restaurants'>
+        <div className='restaurant-actions'>
+          <div className="left-section">
+            <a href='#' className='btn btn-secondary' onClick={this.toggleFilters}>
+              <i className='fas fa-sliders-h'></i> Filter
+            </a>
+            {
+              showFilters &&
+              <div className='filters-pop-up pop-up'>
+                <div>Filters</div>
+                <div className='field'>
+                  <input
+                    id='filter-pickup'
+                    name="filterPickup"
+                    type="checkbox"
+                    checked={filterPickup}
+                    onChange={this.handleInputChange} />
+                  <label htmlFor='filter-pickup'>Pickup</label>
+                </div>
+                <div className='field'>
+                  <input
+                    id='filter-delivery'
+                    name="filterDelivery"
+                    type="checkbox"
+                    checked={filterDelivery}
+                    onChange={this.handleInputChange} />
+                  <label htmlFor='filter-delivery'>Delivery</label>
+                </div>
+                <div className='field'>
+                  <input
+                    id='filter-uber-eats'
+                    name="filterUberEats"
+                    type="checkbox"
+                    checked={filterUberEats}
+                    onChange={this.handleInputChange} />
+                  <label htmlFor='filter-uber-eats'>Uber Eats</label>
+                </div>
+                <div className='field'>
+                  <input
+                    id='filter-skip-the-dishes'
+                    name="filterSkipTheDishes"
+                    type="checkbox"
+                    checked={filterSkipTheDishes}
+                    onChange={this.handleInputChange} />
+                  <label htmlFor='filter-skip-the-dishes'>Skip the Dishes</label>
+                </div>
+                <div className='field'>
+                  <input
+                    id='filter-foodora'
+                    name="filterFoodora"
+                    type="checkbox"
+                    checked={filterFoodora}
+                    onChange={this.handleInputChange} />
+                  <label htmlFor='filter-foodora'>Foodora</label>
+                </div>
+                <div className='field'>
+                  <input
+                    id='filter-door-dash'
+                    name="filterDoorDash"
+                    type="checkbox"
+                    checked={filterDoorDash}
+                    onChange={this.handleInputChange} />
+                  <label htmlFor='filter-door-dash'>DoorDash</label>
+                </div>
+                <div>
+                  <a href="#" onClick={(e) => { e.preventDefault(); this.loadRestaurants()}} className='btn btn-primary'>Submit</a>
+                </div>
+              </div>
+            }
+          </div>
+          <div className='right-section'>
+            <a href='/tickets/new' className='btn btn-secondary'>
+              <i className='far fa-envelope'></i> Submit Feedback
+            </a>
+            {' '}
+            <a href="/restaurants/new" className='btn btn-primary'>
+              <i className='fas fa-plus'></i> Add restaurant
+            </a>
+          </div>
+        </div>
         <div className='restaurants'>
           {
             loadingRestaurants &&
