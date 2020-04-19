6afac95178611756c4bbba429c994ff9d6b1d339
Aaron <aaron@sadhankar.com>
2020-04-19 17:00:38 -0400

    get classname directly from props

--- app/javascript/components/pill.jsx
+++ app/javascript/components/pill.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 class Pill extends React.Component {
   render(){
-    const { className, url, text } = this.props;
-    const StylingClassName = `component-pill ${className}`;
+    const { url, text } = this.props;
+    const className = `component-pill ${this.props.className}`;
     return (
       (!url || (url == null) || (url.length == 0)) ?
-        <div className={StylingClassName}>
+        <div className={className}>
           {text}
         </div>  
       :
-        <a className={StylingClassName} href={url} target="_blank">
+        <a className={className} href={url} target="_blank">
           {text} <i className="fas fa-arrow-circle-right"></i>
         </a>
     );
